ec75c3293e9349ab59e0ef361e639d2feb7098d9
@nhTuan <tuan30vh@gmail.com>
2022-12-02 11:35:55 +0700

    update

--- js/scriptSolar.js
+++ js/scriptSolar.js
@@ -7,4 +7,3 @@ document.querySelector(".universe-solar").addEventListener('click', function() {
     const state = document.querySelector(".universe-solar").style.animationPlayState || 'running';
 	document.querySelector(".universe-solar").style.animationPlayState = state === 'running' ? 'paused' : 'running';
 });
-
