3158b060a6894eca243bbb194cb778244d9c5bbe
@nhTuan <tuan30vh@gmail.com>
2022-12-03 21:43:35 +0700

    update

--- js/scriptLunar.js
+++ js/scriptLunar.js
@@ -1,4 +1,12 @@
-document.querySelector("#stop-eclipse").addEventListener('click', function() {
-    const state = document.querySelector(".sun-eclipse").style.animationPlayState || 'running';
-	document.querySelector(".sun-eclipse").style.animationPlayState = state === 'running' ? 'paused' : 'running';
+document.querySelector("#pause-lunar").addEventListener('click', function() {
+    const state = document.querySelector(".earth-eclipse").style.animationPlayState || 'running';
+	document.querySelector(".earth-eclipse").style.animationPlayState = state === 'running' ? 'paused' : 'running';
+});
+document.querySelector("#show-infor").addEventListener('click', function() {
+    const state = document.querySelector(".moon-eclipse dl.infos").style.opacity || '1';
+	document.querySelector(".moon-eclipse dl.infos").style.opacity = state === '1' ? '0' : '1';
+});
+document.querySelector("#show-infor").addEventListener('click', function() {
+    const state = document.querySelector(".earth-eclipse dl.infos").style.opacity || '1';
+	document.querySelector(".earth-eclipse dl.infos").style.opacity = state === '1' ? '0' : '1';
 });
--- js/scriptSolar.js
+++ js/scriptSolar.js
@@ -3,7 +3,15 @@ document.querySelector("#pause-solar").addEventListener('click', function() {
     const state = document.querySelector(".moon-solar").style.animationPlayState || 'running';
 	document.querySelector(".moon-solar").style.animationPlayState = state === 'running' ? 'paused' : 'running';
 });
-document.querySelector(".universe-solar").addEventListener('click', function() {
+document.querySelector("#pause-solar").addEventListener('click', function() {
     const state = document.querySelector(".universe-solar").style.animationPlayState || 'running';
 	document.querySelector(".universe-solar").style.animationPlayState = state === 'running' ? 'paused' : 'running';
+});
+document.querySelector("#show-infor").addEventListener('click', function() {
+    const state = document.querySelector(".sun-solar dl.infos").style.opacity || '1';
+	document.querySelector(".sun-solar dl.infos").style.opacity = state === '1' ? '0' : '1';
+});
+document.querySelector("#show-infor").addEventListener('click', function() {
+    const state = document.querySelector(".moon-solar dl.infos").style.opacity || '1';
+	document.querySelector(".moon-solar dl.infos").style.opacity = state === '1' ? '0' : '1';
 });
